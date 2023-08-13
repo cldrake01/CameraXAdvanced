39860dd4596b0deb5feae133acdbffdf895d6333
Collin Drake <collinlindendrake@gmail.com>
2023-08-13 14:02:06 -0600

    Update MLKitVisionImage.kt

--- tflite/src/main/java/com/example/android/camerax/tflite/MLKitVisionImage.kt
+++ tflite/src/main/java/com/example/android/camerax/tflite/MLKitVisionImage.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.example.mlkit.kotlin
+package com.example.android.camerax.tflite
 
 import android.app.Activity
 import android.content.Context
